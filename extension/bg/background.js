5e8f0e2de13da8c131f0dca3f80d9d0200337398
Yuri Feldman <yuri@tbqh.net>
2016-08-30 11:16:04 +1000

    Reorganise for extension compilation

---
+++ extension/bg/background.js
@@ -0,0 +1,23 @@
+var possibleRepoRegex = /^https:\/\/github.com\/([0-9a-z-]+)\/([0-9a-z-_]+)\/?$/i;
+var tabToMessage = {};
+chrome.webRequest.onCompleted.addListener(function (response) {
+    if (possibleRepoRegex.test(response.url) && response.statusCode === 404) {
+        var matches = possibleRepoRegex.exec(response.url);
+        if (matches === null)
+            return;
+        if (matches.length === 3) {
+            var message_1 = {
+                owner: matches[1],
+                name: matches[2]
+            };
+            tabToMessage[response.tabId] = function () {
+                chrome.tabs.sendMessage(response.tabId, message_1);
+            };
+        }
+    }
+}, { urls: ["https://github.com/*/*"] });
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
+    if (info.status === "complete" && tabId in tabToMessage) {
+        tabToMessage[tabId]();
+    }
+});
